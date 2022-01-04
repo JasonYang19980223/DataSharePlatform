fc69249a8ce6fa6dd25d9ac1deee1dad4f3dd750
JasonYang19980223 <jason0223yang@gmail.com>
2022-01-04 18:15:59 +0800

    add CreatePage comments

--- src/components/Pages/CreatePage.js
+++ src/components/Pages/CreatePage.js
@@ -4,8 +4,16 @@ import Create from './Create.js'
 import platform from '../Load/platform.js'
 import Nbar from '../Nbar.js';
 
-class CreatePage extends Component {
 
+//********創建成員的介面***********
+
+class CreatePage extends Component {
+  //account 使用者的地址
+  //manager 發布合約的地址
+  //name 組織註冊的名稱
+  //phone 組織註冊的電話
+  //email 組織註冊的信箱
+  //isLogIn 該地址是否已成為成員
   constructor(props){
     super(props)
     this.state = {
@@ -17,6 +25,9 @@ class CreatePage extends Component {
       isLogIn:false
     }    
   }
+
+  //進入頁面前先進行初始化，設定使用者地址，判斷是否為管理者
+  //呼叫function check來判定是否已註冊成員
   async componentWillMount() {
     const accounts = await web3.eth.getAccounts()
     this.setState({account: accounts[0] })
@@ -30,9 +41,11 @@ class CreatePage extends Component {
     await this.check()
   }
 
+  //呼叫合約中的memberCheck這個mapping來判定該組織是否已成為成員
   async check(){
     let log =await platform.methods.memberCheck(this.state.account).call()
     this.setState({isLogIn:log})
+    //若已成為成員，頁面顯示成員的基本資料
     if(this.state.isLogIn){
       let mem = await platform.methods.members(this.state.account).call()
       this.setState({name:mem.orgnizationName})
@@ -41,9 +54,12 @@ class CreatePage extends Component {
     }
   }
 
+  //頁面互動顯示程式碼
   render() {
     let page;
+    //利用isLogIn變數來決定顯示的介面為何
     if(this.state.isLogIn!==true){
+      //顯示Create.js
       page=<Create/>;
     }
     else{
